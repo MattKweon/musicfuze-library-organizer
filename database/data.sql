2f5f61c2d61f8bedb2ae2c45cd07cd665426de4a
Matthew Kweon <kweon.matt@gmail.com>
2022-11-08 00:56:28 +0000

    Add dummy data to data.sql in order to pre-populate demo user account.

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
--- insert into "accounts" ("username", "hashedPassword")
--- values ('anonymous',)
+insert into "accounts" ("username", "hashedPassword")
+values ('anonymous', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA')
