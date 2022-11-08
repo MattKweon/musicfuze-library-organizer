684b697669585fe3e3ea2be0f48eb70a0a1793dc
Matthew Kweon <kweon.matt@gmail.com>
2022-11-08 00:54:49 +0000

    Import DB Designer sql code to schema.sql.

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,68 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."accounts" (
+	"accountId" serial NOT NULL,
+	"username" TEXT NOT NULL,
+	"hashedPassword" TEXT NOT NULL,
+	"savedSongs" int NOT NULL,
+	CONSTRAINT "accounts_pk" PRIMARY KEY ("accountId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."tracks" (
+	"trackId" int NOT NULL,
+	"name" TEXT NOT NULL,
+	"artist" TEXT NOT NULL,
+	"artistPictureUrl" TEXT NOT NULL,
+	"album" TEXT NOT NULL,
+	"albumCoverUrl" TEXT NOT NULL,
+	CONSTRAINT "tracks_pk" PRIMARY KEY ("trackId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."playlists" (
+	"playlistId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"accountId" int NOT NULL,
+	CONSTRAINT "playlists_pk" PRIMARY KEY ("playlistId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."playlistTracks" (
+	"playlistId" int NOT NULL,
+	"trackId" serial NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."library" (
+	"accountId" int NOT NULL,
+	"trackId" int NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "playlists" ADD CONSTRAINT "playlists_fk0" FOREIGN KEY ("accountId") REFERENCES "accounts"("accountId");
+
+ALTER TABLE "playlistTracks" ADD CONSTRAINT "playlistTracks_fk0" FOREIGN KEY ("playlistId") REFERENCES "playlists"("playlistId");
+ALTER TABLE "playlistTracks" ADD CONSTRAINT "playlistTracks_fk1" FOREIGN KEY ("trackId") REFERENCES "tracks"("trackId");
+
+ALTER TABLE "library" ADD CONSTRAINT "library_fk0" FOREIGN KEY ("accountId") REFERENCES "accounts"("accountId");
+ALTER TABLE "library" ADD CONSTRAINT "library_fk1" FOREIGN KEY ("trackId") REFERENCES "tracks"("trackId");
