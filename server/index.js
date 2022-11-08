bf6685e0f0302577185c734086505157641f3d5f
Matthew Kweon <kweon.matt@gmail.com>
2022-11-08 19:31:56 +0000

    Create a route for a POST request that takes the user's input for username and password.Makes sure that both values were entered correctly. If not, the user receives an error.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,49 @@
 require('dotenv/config');
+const pg = require('pg');
+const argon2 = require('argon2');
 const express = require('express');
+const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
 app.use(staticMiddleware);
+app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "accounts" ("username", "hashedPassword")
+        values ($1, $2)
+        on conflict ("username")
+        do nothing
+        returning "accountId", "username"
+      `;
+      const params = [username, hashedPassword];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [account] = result.rows;
+      if (!account) {
+        throw new ClientError(409, 'username is already taken');
+      }
+      res.status(201).json(account);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
