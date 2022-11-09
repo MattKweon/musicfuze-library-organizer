af9d4c000e3690aa75666dec9e2bf3b8eb18cabd
Matthew Kweon <kweon.matt@gmail.com>
2022-11-09 22:05:50 +0000

    Completed issue#1 user-can-create-an-account.

--- client/pages/auth.jsx
+++ client/pages/auth.jsx
@@ -5,7 +5,7 @@ export default class Auth extends React.Component {
   render() {
     return (
       <div className="row pt-5 align-items-center">
-        <div className="col-sm-10 offset-sm-1 col-lg-8 offset-lg-2">
+        <div className=" col-12 offset-0 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-6 offset-xl-3">
           <header className="text-center">
             <h1 className="mb-2">MusicFuze</h1>
             <p className="text-muted mb-4">Create an account to get started!</p>
