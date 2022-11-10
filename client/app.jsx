d1f8067e936593cd55605108585aa79b8d1b1359
Matthew Kweon <kweon.matt@gmail.com>
2022-11-10 01:49:51 +0000

    saving progress: left off at setting up the initial route to load to mount the page properly

--- client/app.jsx
+++ client/app.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import parseRoute from './lib/parse-route';
-import Auth from './pages/auth';
+import AppContext from './lib/app-context';
+// import parseRoute from './lib/parse-route';
+import AuthPage from './pages/auth-page';
 import Navbar from './components/navbar';
 import PageContainer from './components/page-container';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      route: parseRoute(window.location.hash)
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       user: null,
+//       route: parseRoute(window.location.hash)
+//     };
+//   }
 
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      const newRoute = parseRoute(window.location.hash);
-      this.setState({ route: newRoute });
-    });
-  }
+  //   componentDidMount() {
+  //     window.addEventListener('hashchange', () => {
+  //       const newRoute = parseRoute(window.location.hash);
+  //       this.setState({ route: newRoute });
+  //     });
+  //   }
 
   render() {
     return (
-      <>
-        <Navbar />
-        <PageContainer>
-          <Auth />
-        </PageContainer>
-      </>
+      <AppContext.Provider>
+        <>
+          <Navbar />
+          <PageContainer>
+            <AuthPage />
+          </PageContainer>
+        </>
+      </AppContext.Provider>
     );
   }
 }
--- client/pages/auth-page.jsx
+++ client/pages/auth-page.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 import AuthForm from '../components/auth-form';
 
-export default class Auth extends React.Component {
+export default class AuthPage extends React.Component {
   render() {
+    // const { route } = this.context;
+    // const welcomeMessage = route.path === 'sign-in'
+    //   ? 'Please sign-in to continue'
+    //   : 'Create an account to get started!';
     return (
       <div className="row pt-5 align-items-center">
         <div className=" col-12 offset-0 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-6 offset-xl-3">
           <header className="text-center">
             <h1 className="mb-2">MusicFuze</h1>
-            <p className="text-muted mb-4">Create an account to get started!</p>
+            <p className="text-muted mb-4">blahblah</p>
           </header>
           <div className="card p-3">
-            <AuthForm />
+            <AuthForm
+              // action={route.path}
+            />
           </div>
         </div>
       </div>
     );
   }
 }
+
+AuthPage.contextType = AppContext;
