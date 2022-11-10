aa9596fd6d4fb76edbed96481bb5b0d8c00ba71c
Matthew Kweon <kweon.matt@gmail.com>
2022-11-10 01:18:08 +0000

    Event listeners should be written in the ComponentDidMount function in the app component.

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import parseRoute from './lib/parse-route';
 import Auth from './pages/auth';
 import Navbar from './components/navbar';
 import PageContainer from './components/page-container';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const newRoute = parseRoute(window.location.hash);
+      this.setState({ route: newRoute });
+    });
+  }
+
   render() {
     return (
       <>
