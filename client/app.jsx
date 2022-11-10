ba2c1760fe8af08ce945e00942ef83f00d8e9dcd
Matthew Kweon <kweon.matt@gmail.com>
2022-11-10 19:58:17 +0000

    Successfully render the home page when a user logins in with a valid account.

--- client/app.jsx
+++ client/app.jsx
@@ -15,6 +15,8 @@ export default class App extends React.Component {
       isAuthorizing: true,
       route: parseRoute(window.location.hash)
     };
+
+    this.handleSignIn = this.handleSignIn.bind(this);
   }
 
   componentDidMount() {
