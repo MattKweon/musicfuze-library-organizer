2067f2b9cb399e72967921043821ac6cd1d4c3cd
Matthew Kweon <kweon.matt@gmail.com>
2022-11-17 03:32:55 +0000

    saving progress: the change in window.location should trigger componentDidUpdated, resulting in a rerender/showing the list of songs.

--- client/pages/home-page.jsx
+++ client/pages/home-page.jsx
@@ -3,14 +3,31 @@ import Redirect from '../components/redirect';
 import AppContext from '../lib/app-context';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { dummy: false };
+
+    this.handleCategoryClick = this.handleCategoryClick.bind(this);
+  }
+
+  handleCategoryClick(e) {
+    e.preventDefault();
+    const categoryType = e.target.closest('[data-category]').getAttribute('data-category');
+    let newHash = '#?';
+    newHash += `libCategory=${categoryType}`;
+    window.location.hash = newHash;
+  }
+
   render() {
+    const { handleCategoryClick } = this;
+
     if (!this.context.user) return <Redirect to="sign-in" />;
 
     return (
       <>
         <h1 className="fw-bolder">Library</h1>
-        <div className="container">
-          <div className="row clickable-row" href="#?libCategory=songs">
+        <div className="container" onClick={handleCategoryClick}>
+          <div data-category="songs" className="row clickable-row">
             <div className="col-1 p-0">
               <span className="material-symbols-outlined">
                 music_note
@@ -25,6 +42,7 @@ export default class Home extends React.Component {
               </span>
             </div>
           </div>
+          <hr className="style1" />
         </div>
       </>
     );
